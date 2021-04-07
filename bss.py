145458f912537131e2ed2b993688f04ef31044a0
mmcenta <matheuscenta@gmail.com>
2021-04-07 04:00:56 +0200

    Update BSS

--- bss.py
+++ bss.py
@@ -6,9 +6,9 @@ import numpy as np
 
 
 class Node:
-    def __init__(self, type, belstate, depth, edge=None):
+    def __init__(self, type, hyperstate, depth, edge=None):
         self.type = type
-        self.belstate = belstate
+        self.hyperstate = hyperstate
         self.depth = depth
         self.edge = edge
         self.children = {}
@@ -37,7 +37,7 @@ def _get_index(state, action, next_state, info):
             next_state)
 
 
-def init_belstate(state, info, prior_alphas=None):
+def init_hyperstate(state, info, prior_alphas=None):
     n_params = info['n_states'] * info['n_actions'] * info['n_states']
     if not prior_alphas:
         prior_alphas = np.ones(n_params, dtype=np.long)
@@ -45,8 +45,8 @@ def init_belstate(state, info, prior_alphas=None):
     return (prior_alphas, state, counts)
 
 
-def sample(belstate, action, info):
-    alphas, state, _ = belstate
+def sample(hyperstate, action, info):
+    alphas, state, _ = hyperstate
 
     # sample next state
     start = _get_index(state, action, 0, info)
@@ -60,8 +60,8 @@ def sample(belstate, action, info):
     return reward, state
 
 
-def update_posterior(belstate, action, next_state, info):
-    alphas, state, counts = belstate
+def update_posterior(hyperstate, action, next_state, info):
+    alphas, state, counts = hyperstate
 
     idx = _get_index(state, action, next_state, info)
     new_alphas = alphas.copy()
@@ -72,8 +72,8 @@ def update_posterior(belstate, action, next_state, info):
     return (new_alphas, next_state, new_counts)
 
 
-def thompson_sample(belstate, info):
-    alphas, state, _ = belstate
+def thompson_sample(hyperstate, info):
+    alphas, state, _ = hyperstate
 
     best_action, best_q_value = None, float('-inf')
     for action in range(info['n_actions']):
@@ -94,8 +94,8 @@ def thompson_sample(belstate, info):
     return best_action
 
 
-def max_expected_reward(belstate, info):
-    alphas, state, _ = belstate
+def max_expected_reward(hyperstate, info):
+    alphas, state, _ = hyperstate
 
     max_rew, best_action = float('-inf'), None
     for action in range(info['n_actions']):
@@ -116,9 +116,14 @@ def max_expected_reward(belstate, info):
 
 
 def eval_tree(root, horizon, info):
+    """
+    Evaluates (computes state value estimates) the bayesian sparse subtree 
+    rooted at 'root'. Returns both the state value and the optimal action for
+    decision nodes.
+    """
     if not root.children:
         assert root.type == 'decision', 'All leaf nodes are decision nodes'
-        immed, best_action = max_expected_reward(root.belstate, info)
+        immed, best_action = max_expected_reward(root.hyperstate, info)
         return immed * (horizon - root.depth), best_action
 
     if root.type == 'decision':
@@ -138,7 +143,13 @@ def eval_tree(root, horizon, info):
 
 
 def grow_sparse_bayesian_tree(root, budget, branch_proba, horizon, info):
-    for _ in range(budget):
+    """
+    Grows a bayesian sparse tree from a root node by adding 'budget' nodes
+    according to the Bayesian Sparse Sampling algorithm.
+    Returns the evaluation of the subtree starting at 'root'.
+    """
+    num_nodes = 1
+    while num_nodes < budget:
         branch_node, edge_value = bayes_descent(root, branch_proba, info)
 
         if branch_node.depth >= horizon:
@@ -148,86 +159,95 @@ def grow_sparse_bayesian_tree(root, budget, branch_proba, horizon, info):
             action = edge_value
 
             # add outcome node
-            outcome_node = Node('outcome', branch_node.belstate, branch_node.depth, action)
+            outcome_node = Node('outcome', branch_node.hyperstate, branch_node.depth, action)
             branch_node.children[action] = outcome_node
 
             # add decision node
-            rew, next_state = sample(branch_node.belstate, action, info)
-            posterior = update_posterior(branch_node.belstate, action, next_state, info)
+            rew, next_state = sample(branch_node.hyperstate, action, info)
+            posterior = update_posterior(branch_node.hyperstate, action, next_state, info)
 
             decision_node = Node('decision', posterior, branch_node.depth + 1, (rew, next_state))
             outcome_node.children[(rew, next_state)] = decision_node
+
+            num_nodes += 2
     
         elif branch_node.type == 'outcome':
             # add decision node
             rew, next_state = edge_value
-            posterior = update_posterior(branch_node.belstate, action, next_state, info)
+            posterior = update_posterior(branch_node.hyperstate, action, next_state, info)
 
             decision_node = Node('decision', posterior, branch_node.depth + 1, (rew, next_state))
             branch_node.children[(rew, next_state)] = decision_node
+
+            num_nodes += 1
     
     return eval_tree(root, horizon, info)
 
 
 def bayes_descent(node, branch_proba, info):
+    """
+    Descends on an exisiting bayesian sparse tree and chooses a new node to add
+    to the tree.
+    """
     if node.type == 'decision':
-        a = thompson_sample(node.belstate, info)
+        a = thompson_sample(node.hyperstate, info)
         if a not in node.children: # expand tree
             return node, a
         else: # go deeper
             return bayes_descent(node.children[a], branch_proba, info)
 
     if node.type == 'outcome':
-        rew, next_state = sample(node.belstate, node.edge, info)
+        rew, next_state = sample(node.hyperstate, node.edge, info)
         if (random.random() < branch_proba
             or (rew, next_state) not in node.children): # with probability branch_proba, start a new branch here
             return node, (rew, next_state)
         
-        # otherwise, follow
+        # otherwise, go deeper
         return bayes_descent(node.children[(rew, next_state)], branch_proba, info)
 
 
-def bss_planning(belstate, budget, branch_proba, horizon, info):
-    root = Node('decision', belstate, 0)
-    return grow_sparse_bayesian_tree(root, budget, branch_proba, horizon, info)
+class BSSAgent:
+    def __init__(self, budget, branch_proba, horizon, info):
+        self.budget = budget
+        self.branch_proba = branch_proba
+        self.horizon = horizon
+        self.info = info
+
+    def act(self, hyperstate):
+        root = Node('decision', hyperstate, 0)
+        value, action = grow_sparse_bayesian_tree(root, self.budget,
+            self.branch_proba, self.horizon, self.info)
+        return action, {'value': value, 'root': root}
 
 
 if __name__ == "__main__":
+    import argparse
     import gym
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--budget', '-b', type=int, default=200,
+        help='Number of nodes to explore per planning step.')
+    parser.add_argument('--branch-proba', '-bp', type=float, default=0.0,
+        help='Probability to create a new random branch on an outcome node.')
+    parser.add_argument('--horizon', '-h', type=int, default=5,
+        help='Maximum planning depth.')
+    args = parser.parse_args()
 
     env = gym.make('NChain-v0')
     info = get_info(env)
     
-    print(info)
+    print('Enviroment Information:\n {}'.format(info))
 
-    state = env.reset()
-    root = Node('decision', init_belstate(state, info), 0)
-    value = grow_sparse_bayesian_tree(root, 10, 0.0, 10, info)
-
-    print(value)
-
-    def print_tree(root, name='r'):
-        print('{}\n'
-              '  type: {}\n'
-              '  counts: {}\n'
-              '  depth: {}\n'
-              '  edge: {}'.format(name, root.type, root.belstate[2], root.depth, root.edge))
-        for i, c in enumerate(root.children.values()):
-            print_tree(c, name=name+str(i))
-
-    print_tree(root)
+    agent = BSSAgent(args.budget, args.branch_proba, args.horizon, info)
 
     rsum, t = 0, 0
     state, done = env.reset(), False
-    belstate = init_belstate(state, info)
+    hyperstate = init_hyperstate(state, info)
     while not done:
-        value, action = bss_planning(belstate, 500, 0.01, 15, info)
-        #action = env.action_space.sample()
+        action, _ = agent.act(hyperstate)
         next_state, rew, done, _ =  env.step(action)
-        belstate = update_posterior(belstate, action, next_state, info)
-        print(rew)
+        hyperstate = update_posterior(hyperstate, action, next_state, info)
         rsum += rew
         t += 1
-
     print(rsum, t, rsum / t)
 
